89f39428444a63dbcb255be755ba2d6534f076bb
Thomas Loja <tloja1git config --global user.email tloja1@binghamton.edu>
2019-08-30 21:26:11 -0400

    final comitt

--- ComputerOwnerTester.java
+++ ComputerOwnerTester.java
@@ -8,10 +8,10 @@ public class ComputerOwnerTester {
 		Person Guy1 = new Person("Jeff", "Diamonds", "123948745", new DateAndPlaceOfBirth(1982, 4, 5, "Atlantic City", "New Jersey", "United States"), new StreetUSAddress("40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112"));
 		
 		ComputerOwner compOwner = new ComputerOwner(Guy1);
-		Computer computer1 = new Computer("MSI", "Apache", 16, 500, true, 800.00);
+		Computer computer1 = new Computer("Dell", "Destroyer", 16, 500, true, 800.00);
 		Computer computer2 = new Computer("Microsoft", "Go", 8, 1100, false, 500.00);
-		Computer computer3 = new Computer("MSI", "GL63", 16, 2000, true, 1500.00);
-		Computer computer4 = new Computer("Apple", "Touch", 16, 300, false, 3000.00);
+		Computer computer3 = new Computer("Dell", "Envy", 16, 2000, true, 1500.00);
+		Computer computer4 = new Computer("Apple", "Mac", 16, 300, false, 3000.00);
 
 		compOwner.addComputer(computer1);
 		compOwner.addComputer(computer2);
@@ -30,10 +30,10 @@ public class ComputerOwnerTester {
 		Person Guy12 = new Person("Jeff", "Diamonds", "123948745", new DateAndPlaceOfBirth(1982, 4, 5, "Atlantic City", "New Jersey", "United States"), new StreetUSAddress("40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112"));
 		
 		ComputerOwner compOwner2 = new ComputerOwner(Guy1);
-		Computer computer12 = new Computer("MSI", "Apache", 16, 500, true, 800.00);
+		Computer computer12 = new Computer("Dell", "Destroyer", 16, 500, true, 800.00);
 		Computer computer22 = new Computer("Microsoft", "Go", 8, 1100, false, 500.00);
-		Computer computer32 = new Computer("MSI", "GL63", 16, 2000, true, 1500.00);
-		Computer computer42 = new Computer("Apple", "Touch", 16, 300, false, 3000.00);
+		Computer computer32 = new Computer("Dell", "Envy", 16, 2000, true, 1500.00);
+		Computer computer42 = new Computer("Apple", "Mac", 16, 300, false, 3000.00);
 
 		compOwner2.addComputer(computer12);
 		compOwner2.addComputer(computer22);
