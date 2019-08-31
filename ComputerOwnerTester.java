ee735416cf48ae836bfd231e7a2e856bc46e8a95
Thomas Loja <tloja1git config --global user.email tloja1@binghamton.edu>
2019-08-30 21:20:07 -0400

    final comitt

--- ComputerOwnerTester.java
+++ ComputerOwnerTester.java
@@ -5,38 +5,54 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 public class ComputerOwnerTester {
 	public static void main(String[] args) {
-		Person Guy1 = Person("Jeff", "Diamonds", 123948745, 1982, 4, 5, "Atlantic City", "New Jersey", "United 			States", "40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112");
-		complist = [];
-		ComputerOwner CompOwner = new ComputerOwner(Guy1, complist);
-		for (i = 0, i<=4, i++){
-		CompOwner.addComputer(Comp)
+		Person Guy1 = new Person("Jeff", "Diamonds", "123948745", new DateAndPlaceOfBirth(1982, 4, 5, "Atlantic City", "New Jersey", "United States"), new StreetUSAddress("40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112"));
 		
-		}
-		system.out.println(CompOwner);		
-		for (i=0,i<=2,i++){
-		CompOwner.removeComputer(i);
-		}
-		system.out.println(CompOwner);
+		ComputerOwner compOwner = new ComputerOwner(Guy1);
+		Computer file1 = new Computer("MSI", "Apache", 16, 500, true, 800.00);
+		Computer file2 = new Computer("Microsoft", "Go", 8, 1100, false, 500.00);
+		Computer file3 = new Computer("MSI", "GL63", 16, 2000, true, 1500.00);
+		Computer file4 = new Computer("Apple", "Touch", 16, 300, false, 3000.00);
+
+		compOwner.addComputer(file1);
+		compOwner.addComputer(file2);
+		compOwner.addComputer(file3);
+		compOwner.addComputer(file4);
+		System.out.println(compOwner);
+		
+		compOwner.removeComputer(2);
+		compOwner.removeComputer(2);
+		System.out.println(compOwner);
+
 		try(var output =new PrintWriter(new FileOutputStream(
 			    new File("output.txt"), true /* true means append to file */))) {
 			output.println("\nTESTS FOR ComputerOwner.java:");
-		Person Guy12 = Person("Jeff", "Diamonds", 123948745, 1982, 4, 5, "Atlantic City", "New Jersey", "United 			States", "40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112");
-		complist2 = [];
-		ComputerOwner CompOwner = new ComputerOwner(Guy12, complist2);
-		for (i = 0, i<=4, i++){
-		CompOwner.addComputer(Comp)
 		
-		}
-		Output.println(CompOwner);		
-		for (i=0,i<=2,i++){
-		CompOwner.removeComputer(i);
-		}
-		Output.println(CompOwner)
+		Person Guy12 = new Person("Jeff", "Diamonds", "123948745", new DateAndPlaceOfBirth(1982, 4, 5, "Atlantic City", "New Jersey", "United States"), new StreetUSAddress("40-53 Dowry Lane", "", "Atlantic City", "NJ", "12112"));
+		
+		ComputerOwner compOwner2 = new ComputerOwner(Guy1);
+		Computer file12 = new Computer("MSI", "Apache", 16, 500, true, 800.00);
+		Computer file22 = new Computer("Microsoft", "Go", 8, 1100, false, 500.00);
+		Computer file32 = new Computer("MSI", "GL63", 16, 2000, true, 1500.00);
+		Computer file42 = new Computer("Apple", "Touch", 16, 300, false, 3000.00);
+
+		compOwner2.addComputer(file12);
+		compOwner2.addComputer(file22);
+		compOwner2.addComputer(file32);
+		compOwner2.addComputer(file42);
+		System.out.println(compOwner2);
+		
+		compOwner2.removeComputer(2);
+		compOwner2.removeComputer(2);
+		System.out.println(compOwner2);
+
+
 
 
 
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+		
 	}
 }
+
